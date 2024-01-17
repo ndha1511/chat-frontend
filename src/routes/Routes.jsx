398c3549d6b4885fd285afcb4844421e509535b3
TranCongMinh172 <trancongminh1722002@gmail.com>
2024-01-17 16:41:37 +0700

    add contact

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,7 +1,12 @@
-import Contact from "../pages/contact/Contac";
+import Contact from "../pages/contact/menu/Menu";
+import Content1 from "../pages/contact/content/Content1";
+
+
 import Home from "../pages/home/Home";
+import ContactLayout from "../components/contactLayout/ContactLayout";
+
 
 export const routes = [
     {path: '/', component: Home},
-    {path: '/contact', component: Contact},
+    {path: '/content1', component: Content1,layout:ContactLayout},
 ];
